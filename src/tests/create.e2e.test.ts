9ac3f5b98f2529789c01891d01e5f47a7692b16e
Bartosz <burtekdotryw@gmail.com>
2026-09-08 09:39:10 +0200

    test: add tests for generator metadata

--- src/tests/create.e2e.test.ts
+++ src/tests/create.e2e.test.ts
@@ -3,71 +3,71 @@ import { mkdtemp, rm } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import { promisify } from "node:util";
+import packageJson from '../../package.json' with { type: 'json' };
+
 
 const exec = promisify(execFile);
 
+const it = test
+    .extend('tarball', { auto: true, scope: 'file' }, () => {
+        assertString(process.env.E2E_TARBALL);
+        return process.env.E2E_TARBALL;
+    })
+    .extend('cwd', { auto: true, scope: 'file' }, async ({}, { onCleanup }) => {
+        const cwd = await mkdtemp(join(tmpdir(), 'create-dtrw-app'));
+        onCleanup(async () => await rm(cwd, { recursive: true, force: true }));
+        return cwd;
+    })
+    .extend('pkg', { auto: true }, async ({ cwd, task }, { onCleanup }) => {
+        const name = `app-${task.id}`;
+        const fullname = `dtrw-app-${name}`;
+        return { name, fullname, root: join(cwd, fullname) };
+    })
+    .extend('execCreate', { auto: true }, ({ cwd, tarball }) => async (...args: string[]) => {
+        return await exec('pnpm', ['--config.dlx-cache-max-age=0', 'dlx', tarball, ...args], {
+            cwd, env: process.env
+        });
+    })
+
 describe('creating new project', () => {
     let tarball: string;
-    let cwd: string;
 
-    beforeAll(() => {
+    beforeAll(async () => {
         assertString(process.env.E2E_TARBALL);
         tarball = process.env.E2E_TARBALL;
     });
 
-    beforeEach(async () => {
-        cwd = await mkdtemp(join(tmpdir(), 'create-dtrw-app'));
-    });
-
-    const outputs = new Map<string, string>();
-    afterEach(async ctx => {
-        if (ctx.task.result?.state === 'pass') {
-            await rm(cwd, { recursive: true, force: true });
-        } else if (ctx.task.result?.state === 'fail') {
-            outputs.set(ctx.task.fullTestName, cwd);
-        }
-    });
-
-    afterAll(() => {
-        outputs.forEach((value, key) => {
-            console.error(key);
-            console.error('  ' + value);
-        })
-    })
-
-    async function execCreate(...args: string[]) {
-        const result = await exec('pnpm', ['--config.dlx-cache-max-age=0', 'dlx', tarball, ...args], {
-            cwd, env: process.env
-        });
-
-        return { cwd, result };
+    async function expectGitCommit(cwd: string, message: string) {
+        await expect(exec('git', ['log', '--oneline'], { cwd }))
+            .resolves
+            .toMatchObject({ stderr: '', stdout: expect.stringMatching(message) });
     }
-
-    it('should create empty project', async () => {
-        const { result: { stdout } } = await execCreate('testapp', '--no-create-github-repo');
+``
+    it('should create empty project', async ({ execCreate, pkg }) => {
+        const { stdout } = await execCreate(pkg.name, '--no-create-github-repo');
 
         expect(stdout).toMatch('Creating project root');
         expect(stdout).toMatch('Installing dependencies');
         expect(stdout).toMatch('Skipping tests because no packages were added');
         expect(stdout).toMatch('Committing files');
 
-        await expect(exec('git', ['log', '--oneline'], { cwd: join(cwd, 'dtrw-app-testapp') }))
-            .resolves
-            .toStrictEqual({
-                stderr: '',
-                stdout: expect.stringMatching('feat: new project dtrw-app-testapp created')
-            });
+        await expectGitCommit(pkg.root, `feat: new project ${pkg.fullname} created`);
 
-        expect(join(cwd, 'dtrw-app-testapp', 'package.json')).toBeFile();
-        expect(join(cwd, 'dtrw-app-testapp', 'packages')).toBeEmptyDirectory();
+        expect(join(pkg.root, 'package.json')).toMatchFileObject({
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templatesUsed: {}
+            }
+        });
+        expect(join(pkg.root, 'packages')).toBeEmptyDirectory();
 
-        expect(join(cwd, 'dtrw-app-testapp', 'docker-compose.yml')).toMatchFileObject({
+        expect(join(pkg.root, 'docker-compose.yml')).toMatchFileObject({
             services: {}
         });
     });
 
-    it('should create project with provided packages', async () => {
-        const { result: { stdout } } = await execCreate('testapp-with-packages', '--no-create-github-repo', '-w', 'reactjs:ui', '-w', 'nodejs:api');
+    it('should create project with provided packages', async ({ execCreate, pkg }) => {
+        const { stdout } = await execCreate(pkg.name, '--no-create-github-repo', '-w', 'reactjs:ui', '-w', 'nodejs:api');
 
         expect(stdout).toMatch('Creating project root');
         expect(stdout).toMatch('Creating package ui using preset reactjs');
@@ -76,35 +76,46 @@ describe('creating new project', () => {
         expect(stdout).toMatch('Running tests');
         expect(stdout).toMatch('Committing files');
 
-        await expect(exec('git', ['log', '--oneline'], { cwd: join(cwd, 'dtrw-app-testapp-with-packages') }))
-            .resolves
-            .toStrictEqual({
-                stderr: '',
-                stdout: expect.stringMatching('feat: new project dtrw-app-testapp-with-packages created')
-            });
+        await expectGitCommit(pkg.root, `feat: new project ${pkg.fullname} created`);
 
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'package.json')).toBeFile();
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'packages')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'packages', 'ui')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'packages', 'api')).toBeDirectory();
+        expect(join(pkg.root, 'package.json')).toMatchFileObject({
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templatesUsed: {
+                    ui: 'reactjs',
+                    api: 'nodejs'
+                }
+            }
+        });
+        expect(join(pkg.root, 'packages')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'ui')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'api')).toBeDirectory();
 
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'docker-compose.yml')).toMatchFileObject({
+        expect(join(pkg.root, 'docker-compose.yml')).toMatchFileObject({
             services: {
-                api: expect.objectContaining({ container_name: 'testapp-with-packages_api' }),
-                ui: expect.objectContaining({ container_name: 'testapp-with-packages_ui' }),
+                api: expect.objectContaining({ container_name: `${pkg.name}_api` }),
+                ui: expect.objectContaining({ container_name: `${pkg.name}_ui` }),
             }
         });
 
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'packages', 'ui', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ react: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'ui', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ react: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'reactjs'
+            }
         });
-        expect(join(cwd, 'dtrw-app-testapp-with-packages', 'packages', 'api', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ fastify: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'api', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ fastify: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'nodejs'
+            }
         });
     });
 
-    it('should create project using starter', async () => {
-        const { result: { stdout } } = await execCreate('testapp-using-starter', '--no-create-github-repo', '-s', 'fullstack');
+    it('should create project using starter', async ({ execCreate, pkg }) => {
+        const { stdout } = await execCreate(pkg.name, '--no-create-github-repo', '-s', 'fullstack');
 
         expect(stdout).toMatch('Creating project root');
         expect(stdout).toMatch('Creating package frontend using preset reactjs');
@@ -113,35 +124,46 @@ describe('creating new project', () => {
         expect(stdout).toMatch('Running tests');
         expect(stdout).toMatch('Committing files');
 
-        await expect(exec('git', ['log', '--oneline'], { cwd: join(cwd, 'dtrw-app-testapp-using-starter') }))
-            .resolves
-            .toStrictEqual({
-                stderr: '',
-                stdout: expect.stringMatching('feat: new project dtrw-app-testapp-using-starter created')
-            });
+        await expectGitCommit(pkg.root, `feat: new project ${pkg.fullname} created`);
 
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'package.json')).toBeFile();
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'packages')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'packages', 'frontend')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'packages', 'backend')).toBeDirectory();
+        expect(join(pkg.root, 'package.json')).toMatchFileObject({
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templatesUsed: {
+                    frontend: 'reactjs',
+                    backend: 'nodejs'
+                }
+            }
+        });
+        expect(join(pkg.root, 'packages')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'frontend')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'backend')).toBeDirectory();
 
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'docker-compose.yml')).toMatchFileObject({
+        expect(join(pkg.root, 'docker-compose.yml')).toMatchFileObject({
             services: {
-                backend: expect.objectContaining({ container_name: 'testapp-using-starter_backend' }),
-                frontend: expect.objectContaining({ container_name: 'testapp-using-starter_frontend' }),
+                backend: expect.objectContaining({ container_name: `${pkg.name}_backend` }),
+                frontend: expect.objectContaining({ container_name: `${pkg.name}_frontend` }),
             }
         });
 
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'packages', 'frontend', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ react: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'frontend', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ react: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'reactjs'
+            }
         });
-        expect(join(cwd, 'dtrw-app-testapp-using-starter', 'packages', 'backend', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ fastify: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'backend', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ fastify: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'nodejs'
+            }
         });
     });
 
-    it('should create project using starter with additional packages', async () => {
-        const { result: { stdout } } = await execCreate('testapp-combo', '--no-create-github-repo', '-s', 'fullstack', '-w', 'reactjs:admin');
+    it('should create project using starter with additional packages', async ({ execCreate, pkg }) => {
+        const { stdout } = await execCreate(pkg.name, '--no-create-github-repo', '-s', 'fullstack', '-w', 'reactjs:admin');
 
         expect(stdout).toMatch('Creating project root');
         expect(stdout).toMatch('Creating package frontend using preset reactjs');
@@ -151,56 +173,72 @@ describe('creating new project', () => {
         expect(stdout).toMatch('Running tests');
         expect(stdout).toMatch('Committing files');
 
-        await expect(exec('git', ['log', '--oneline'], { cwd: join(cwd, 'dtrw-app-testapp-combo') }))
-            .resolves
-            .toStrictEqual({
-                stderr: '',
-                stdout: expect.stringMatching('feat: new project dtrw-app-testapp-combo created')
-            });
+        await expectGitCommit(pkg.root, `feat: new project ${pkg.fullname} created`);
 
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'package.json')).toBeFile();
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'frontend')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'backend')).toBeDirectory();
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'admin')).toBeDirectory();
+        expect(join(pkg.root, 'package.json')).toMatchFileObject({
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templatesUsed: {
+                    frontend: 'reactjs',
+                    backend: 'nodejs',
+                    admin: 'reactjs'
+                }
+            }
+        });
+        expect(join(pkg.root, 'packages')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'frontend')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'backend')).toBeDirectory();
+        expect(join(pkg.root, 'packages', 'admin')).toBeDirectory();
 
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'docker-compose.yml')).toMatchFileObject({
+        expect(join(pkg.root, 'docker-compose.yml')).toMatchFileObject({
             services: {
-                backend: expect.objectContaining({ container_name: 'testapp-combo_backend' }),
-                frontend: expect.objectContaining({ container_name: 'testapp-combo_frontend' }),
-                admin: expect.objectContaining({ container_name: 'testapp-combo_admin' }),
+                backend: expect.objectContaining({ container_name: `${pkg.name}_backend` }),
+                frontend: expect.objectContaining({ container_name: `${pkg.name}_frontend` }),
+                admin: expect.objectContaining({ container_name: `${pkg.name}_admin` }),
             }
         });
 
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'frontend', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ react: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'frontend', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ react: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'reactjs'
+            }
         });
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'backend', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ fastify: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'backend', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ fastify: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'nodejs'
+            }
         });
-        expect(join(cwd, 'dtrw-app-testapp-combo', 'packages', 'admin', 'package.json')).toMatchFileObject({
-            dependencies: expect.objectContaining({ react: expect.any(String) })
+        expect(join(pkg.root, 'packages', 'admin', 'package.json')).toMatchFileObject({
+            dependencies: expect.objectContaining({ react: expect.any(String) }),
+            'create-dtrw-app': {
+                generatorVersion: packageJson.version,
+                templateUsed: 'reactjs'
+            }
         });
     });
 
-    it('should throw if trying to use multiple packages under same name', async () => {
+    it('should throw if trying to use multiple packages under same name', async ({ execCreate, pkg }) => {
         await expect(async () => {
-            await execCreate('testapp-duplicate-names', '--no-create-github-repo', '-w', 'reactjs:app', '-w', 'nodejs:app');
+            await execCreate(pkg.name, '--no-create-github-repo', '-w', 'reactjs:app', '-w', 'nodejs:app');
         }).rejects.toMatchObject({
             message: expect.stringContaining('Error: Package app is defined more than once')
         });
 
-        expect(join(cwd, 'dtrw-app-testapp-duplicate-names')).not.toBeDirectory();
+        expect(pkg.root).not.toBeDirectory();
     });
 
-    it('should throw if trying to use starter and conflicting additional package', async () => {
+    it('should throw if trying to use starter and conflicting additional package', async ({ execCreate, pkg }) => {
         await expect(async () => {
-            await execCreate('testapp-conflicting-package', '--no-create-github-repo', '-s', 'fullstack', '-w', 'reactjs:frontend');
+            await execCreate(pkg.name, '--no-create-github-repo', '-s', 'fullstack', '-w', 'reactjs:frontend');
         }).rejects.toMatchObject({
             message: expect.stringContaining('Error: Starter fullstack already defines package frontend')
         });
 
-        expect(join(cwd, 'dtrw-app-testapp-conflicting-package')).not.toBeDirectory();
+        expect(pkg.root).not.toBeDirectory();
     });
 });
 
